4dd94e25e8d5b0311193b964fb6443a00a11efe7
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-18 00:33:28 +0530

    feat ✨: Enhanced user session and JWT data structures
    - Enhanced user session and JWT data structures to support additional properties and better user role management.
    - The main goal of the changes is to maintain and improve the existing authorization and authentication configuration to handle user sessions and credentials verification in a more efficient and standardized way.
    - The main goal of the changes is to update the user registration functionality to include changes to the phone and password validation, adding new fields for user data, and refactoring the OTP verification and resend processes.

--- @types/next-auth.d.ts
+++ @types/next-auth.d.ts
@@ -1,56 +1,62 @@
 import NextAuth from "next-auth/next";
-declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-      email: string;
-      name: string;
-      image: string;
-    };
-    data: {
-      name: string;
-      dob: string;
-      gender: string;
-      bloodGroup: string;
-      profilePic: string;
-      email: string;
-      phone: string;
+import { EUserRole } from "@/types/user.d.types";
 
-      serviceProvider: {
-        id: string;
-        userId: string;
-        providerType: string;
-        description: string;
-        fee: string;
-        specialization: string[];
-        service: string[];
-      };
-      address: {
-        address: string;
-        city: string;
-        state: string;
-        pincode: string;
-        country: string;
-      };
-      id: string;
-      role: string;
-    };
-    authToken: string;
-  }
+declare module "next-auth" {
+	interface Session {
+		user: {
+			id: string;
+			email: string;
+			name: string;
+			phone: string;
+			role: EUserRole;
+			isVerified: boolean;
+			providerType?: string;
+			address?: {
+				address: string;
+				city: string;
+				state: string;
+				pincode: string;
+				country: string;
+			};
+			profileImage?: string;
+		};
+	}
+
+	interface User {
+		id: string;
+		email: string;
+		name: string;
+		phone: string;
+		role: EUserRole;
+		isVerified: boolean;
+		providerType?: string;
+		address?: {
+			address: string;
+			city: string;
+			state: string;
+			pincode: string;
+			country: string;
+		};
+		profileImage?: string;
+	}
 }
 
-import { JWT } from "next-auth/jwt";
-
 declare module "next-auth/jwt" {
-  interface JWT {
-    user: {
-      id: string;
-      email: string;
-      name: string;
-      image: string;
-    };
-
-    data: any;
-    authToken: string;
-  }
+	interface JWT {
+		id: string;
+		email: string;
+		name: string;
+		phone: string;
+		role: EUserRole;
+		isVerified: boolean;
+		providerType?: string;
+		address?: {
+			address: string;
+			city: string;
+			state: string;
+			pincode: string;
+			country: string;
+		};
+		profileImage?: string;
+	}
 }
--- app/api/auth/[...nextauth]/auth.config.ts
+++ app/api/auth/[...nextauth]/auth.config.ts
@@ -5,137 +5,128 @@ import { loginUser } from "@/server-actions/user";
 import { EUserRole } from "@/types/user.d.types";
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-  throw new Error("Missing Google OAuth credentials");
+	throw new Error("Missing Google OAuth credentials");
 }
 
 export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-      profile(profile) {
-        return {
-          id: profile.sub,
-          email: profile.email,
-          name: profile.name,
-          firstName: profile.given_name,
-          lastName: profile.family_name,
-          image: profile.picture,
-          role: EUserRole.PATIENT,
-          isVerified: true,
-          phone: "",
-        };
-      },
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        phone: { label: "Phone", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.phone || !credentials?.password) {
-          throw new Error("Missing credentials");
-        }
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			authorization: {
+				params: {
+					prompt: "consent",
+					access_type: "offline",
+					response_type: "code",
+				},
+			},
+			profile(profile) {
+				return {
+					id: profile.sub,
+					email: profile.email,
+					name: profile.name,
+					firstName: profile.given_name,
+					lastName: profile.family_name,
+					image: profile.picture,
+					role: EUserRole.PATIENT,
+					isVerified: true,
+					phone: "",
+				};
+			},
+		}),
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				phone: { label: "Phone", type: "text" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials) {
+				if (!credentials?.phone || !credentials?.password) {
+					throw new Error("Missing credentials");
+				}
 
-        const res = await loginUser(credentials.phone, credentials.password);
+				const res = await loginUser(credentials.phone, credentials.password);
 
-        if (res.status !== 201) {
-          throw new Error(res.message || "Authentication failed");
-        }
+				if (res.status !== 201) {
+					throw new Error(res.message || "Authentication failed");
+				}
 
-        return {
-          id: res.user?.id || "",
-          email: res.user?.email || "",
-          name: res.user?.firstName
-            ? `${res.user.firstName} ${res.user.lastName}`
-            : "",
-          phone: res.user?.phone || "",
-          firstName: res.user?.firstName || "",
-          lastName: res.user?.lastName || "",
-          role: res.user?.role || EUserRole.PATIENT,
-          isVerified: res.user?.isVerified || false,
-          image: res.user?.profileImage || null,
-          providerRole: res.user?.providerRole,
-          address: res.user?.address,
-          profileImage: res.user?.profileImage,
-        };
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth/login",
-    error: "/auth/error",
-    newUser: "/",
-  },
-  callbacks: {
-    async jwt({ token, user, account }) {
-      if (user) {
-        // Set user details in the token
-        token.id = user.id;
-        token.email = user.email;
-        token.name = user.name;
-        token.phone = user.phone;
-        token.firstName = user.firstName;
-        token.lastName = user.lastName;
-        token.role = user.role || EUserRole.PATIENT;
-        token.isVerified = user.isVerified;
-        token.providerRole = user.providerRole;
-        token.address = user.address;
-        token.profileImage = user.profileImage;
+				return {
+					id: res.user?.id || "",
+					email: res.user?.email || "",
+					name: res.user?.name || "",
+					phone: res.user?.phone || "",
+					role: res.user?.role || EUserRole.PATIENT,
+					isVerified: res.user?.isVerified || false,
+					image: res.user?.profileImage || null,
+					providerType: res.user?.providerType,
+					address: res.user?.address,
+					profileImage: res.user?.profileImage,
+				};
+			},
+		}),
+	],
+	pages: {
+		signIn: "/auth/login",
+		error: "/auth/error",
+		newUser: "/",
+	},
+	callbacks: {
+		async jwt({ token, user, account }) {
+			if (user) {
+				// Set user details in the token
+				token.id = user.id;
+				token.email = user.email;
+				token.name = user.name;
+				token.phone = user.phone;
+				token.role = user.role || EUserRole.PATIENT;
+				token.isVerified = user.isVerified;
+				token.providerType = user.providerType;
+				token.address = user.address;
+				token.profileImage = user.profileImage;
 
-        if (account) {
-          try {
-            const res = await fetch(`${process.env.SERVER_URL}/user/signup`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                email: user.email,
-              }),
-            });
+				if (account) {
+					try {
+						const res = await fetch(`${process.env.SERVER_URL}/user/signup`, {
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({
+								email: user.email,
+							}),
+						});
 
-            if (res.status !== 201) {
-              throw new Error("Signup failed");
-            }
+						if (res.status !== 201) {
+							throw new Error("Signup failed");
+						}
 
-            const data = await res.json();
-            token.signupData = data; // Store additional data from the signup response
-          } catch (error) {
-            console.error("Error during signup:", error);
-          }
-        }
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.id;
-        session.user.email = token.email;
-        session.user.name = token.name;
-        session.user.phone = token.phone as string;
-        session.user.firstName = token.firstName as string;
-        session.user.lastName = token.lastName as string;
-        session.user.role = token.role as EUserRole;
-        session.user.isVerified = token.isVerified as boolean;
-        session.user.providerRole =
-          token.providerRole as typeof session.user.providerRole;
-        session.user.address = token.address as typeof session.user.address;
-        session.user.profileImage = token.profileImage as string | undefined;
-      }
-      return session;
-    },
-  },
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
+						const data = await res.json();
+						token.signupData = data; // Store additional data from the signup response
+					} catch (error) {
+						console.error("Error during signup:", error);
+					}
+				}
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			if (session.user) {
+				session.user.id = token.id;
+				session.user.email = token.email;
+				session.user.name = token.name;
+				session.user.phone = token.phone;
+				session.user.role = token.role;
+				session.user.isVerified = token.isVerified;
+				session.user.providerType = token.providerType;
+				session.user.address = token.address;
+				session.user.profileImage = token.profileImage;
+			}
+			return session;
+		},
+	},
+	session: {
+		strategy: "jwt",
+		maxAge: 30 * 24 * 60 * 60, // 30 days
+	},
 };
