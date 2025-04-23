03965cf3bcfc4dc459f58bd83fb22f911b25b0d3
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2025-04-23 16:06:14 +0530

    feat ✨: Enabled server components with React
    - The main goal of the changes is to enable server components with React by adding the client directive, enhancing the rendering of the application by taking advantage of this feature.

--- app/dashboard/doctor/[doctorId]/time-slots/page.tsx
+++ app/dashboard/doctor/[doctorId]/time-slots/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import TimeSlotHeader from '@/components/dashboard/doctors/time-slots/TimeSlotHeader';
 import TimeSlotCalendar from '@/components/dashboard/doctors/time-slots/TimeSlotCalendar';
